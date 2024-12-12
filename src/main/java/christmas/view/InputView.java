9e789d365205986c6b273737ffe98a25840736ec
Yeongjae Kim <sees77770@gmail.com>
2024-12-12 20:15:34 +0900

    feat: 뷰 기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public void read() {
+        Console.readLine();
+    }
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,8 @@
+package christmas.view;
+
+public class OutputView {
+
+    public void print() {
+        System.out.println();
+    }
+}
