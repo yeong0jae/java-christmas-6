0405e307121de5ca418fa20995e7dc505a2f0427
Yeongjae Kim <sees77770@gmail.com>
2024-12-12 20:43:27 +0900

    feat: 메뉴 클래스 추가

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,35 @@
+package christmas.domain;
+
+import java.util.List;
+import java.util.Map;
+
+public enum Menu {
+
+    APPETIZER(List.of(
+            Map.of("양송이 스프", 6_000),
+            Map.of("타파스", 5_500),
+            Map.of("시저샐러드", 8_000)
+    )),
+    MAIN(List.of(
+            Map.of("티본스테이크", 55_000),
+            Map.of("바비큐립", 54_000),
+            Map.of("해산물파스타", 35_000),
+            Map.of("크리스마스파스타", 25_000)
+    )),
+    DESSERT(List.of(
+            Map.of("초코케이크", 15_000),
+            Map.of("아이스크림", 5_000)
+    )),
+    DRINK(List.of(
+            Map.of("제로콜라", 3_000),
+            Map.of("레드와인", 60_000),
+            Map.of("샴페인", 25_000)
+    ));
+
+    private List<Map<String, Integer>> menu;
+
+    Menu(List<Map<String, Integer>> menu) {
+        this.menu = menu;
+    }
+
+}
