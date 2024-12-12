246c696a382eaa31e933f8843e2befb376a08613
Yeongjae Kim <sees77770@gmail.com>
2024-12-12 22:44:51 +0900

    feat: 뱃지 구하기 기능 추가

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -3,11 +3,28 @@ package christmas.domain;
 public enum Badge {
     STAR(5_000),
     TREE(10_000),
-    SANTA(20_000);
+    SANTA(20_000),
+    NONE(0);
 
     private final int discount;
 
     Badge(int discount) {
         this.discount = discount;
+    }
+
+    public static Badge from(int totalDiscount) {
+        if (totalDiscount >= valueOf("STAR").discount
+                && totalDiscount < valueOf("TREE").discount) {
+            return STAR;
+        }
+        if (totalDiscount >= valueOf("STAR").discount
+                && totalDiscount < valueOf("TREE").discount) {
+            return TREE;
+        }
+        if (totalDiscount >= valueOf("TREE").discount
+                && totalDiscount < valueOf("SANTA").discount) {
+            return SANTA;
+        }
+        return NONE;
     }
 }
