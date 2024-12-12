87a74a6d490361a3f1333767fb73468d56630bf3
Yeongjae Kim <sees77770@gmail.com>
2024-12-12 20:45:28 +0900

    feat: 주문 클래스 추가

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+public class Order {
+
+    private final String name;
+    private final int count;
+
+    public Order(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
---
+++ src/main/java/christmas/domain/Orders.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+import java.util.List;
+
+public class Orders {
+
+    private final List<Order> orders;
+
+    public Orders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
