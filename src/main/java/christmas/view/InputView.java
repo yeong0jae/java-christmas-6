cc19d5f4c36f8b76e43a56740291b44cbc9a1a02
Yeongjae Kim <sees77770@gmail.com>
2024-12-12 23:31:52 +0900

    fix: 요구사항 예외 메시지를 이용

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String rawInput = Console.readLine();
         if (!rawInput.matches("\\d+")) {
-            throw new IllegalArgumentException(ErrorMessage.PREFIX + "숫자만 입력해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.PREFIX + "유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
         return Integer.parseInt(rawInput);
     }
