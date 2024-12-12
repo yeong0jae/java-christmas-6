8f4e0c12960127bd3f822d71669bccabb7fecc04
Yeongjae Kim <sees77770@gmail.com>
2024-12-13 00:02:52 +0900

    refactor: 컨트롤러 메서드 분리

--- src/main/java/christmas/ChristmasController.java
+++ src/main/java/christmas/ChristmasController.java
@@ -21,21 +21,34 @@ public class ChristmasController {
     public void run() {
         outputView.printStartMessage();
 
-        int visitDate = retryUntilValid(() -> {
-            int date = inputView.readVisitDate();
-            Calendar.isIn(date);
-            return date;
-        });
+        int visitDate = visit();
 
-        Orders orders = retryUntilValid(() -> {
-            List<String> inputOrders = inputView.readOrders();
-            return new Orders(inputOrders);
-        });
+        Orders orders = order();
         outputView.printOrders(orders.getOrders());
 
         int totalPrice = orders.getTotalPrice();
         outputView.printTotalPrice(totalPrice);
 
+        discount(visitDate, orders, totalPrice);
+    }
+
+    private int visit() {
+        return retryUntilValid(() -> {
+            int date = inputView.readVisitDate();
+            Calendar.isIn(date);
+            return date;
+        });
+    }
+
+    private Orders order() {
+        Orders orders = retryUntilValid(() -> {
+            List<String> inputOrders = inputView.readOrders();
+            return new Orders(inputOrders);
+        });
+        return orders;
+    }
+
+    private void discount(int visitDate, Orders orders, int totalPrice) {
         christmasService.discount(visitDate, orders, totalPrice);
 
         outputView.printDiscount(
@@ -47,9 +60,7 @@ public class ChristmasController {
                 christmasService.isNoDiscount(),
                 christmasService.getBadge()
         );
-
         outputView.printPaymentPrice(christmasService.getPaymentPrice());
-
         outputView.printBadge(christmasService.getBadge());
     }
 
